9b0c138b713efcbb3be3c527db783ff2d701a77a
Andy Mai <andrew.x.mai@gmail.com>
2017-03-12 19:17:16 -0700

    add lets encrypt string again

--- region/views.py
+++ region/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Report view")
+  return HttpResponse('QO5VmmdZPJa0X2NYdPaF8GplsTg2IPekQu5OpZ1mKS8.7vqNsFm7akKXmycD9a6zAMBW4e08Jv6UWOVXdOBy_iw')
