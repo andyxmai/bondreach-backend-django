94465adf1bf4b17ddb36cf36f7b637642ae22179
Andy Mai <andrew.x.mai@gmail.com>
2017-04-04 09:35:52 -0700

    support download contacts

--- contact/api/v1/api.py
+++ contact/api/v1/api.py
@@ -52,6 +52,49 @@ class ContactViewSet(viewsets.ModelViewSet):
     return queryset
 
 
+"""
+This extra endpoint is used because pagination needs to be turned off when downloading
+data.
+Note (Andy): need to fix the duplication
+"""
+class DownloadContactViewSet(viewsets.ModelViewSet):
+  pagination_class = None
+
+  serializer_class = ContactSerializer
+  filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter,)
+  filter_class = ContactFilter
+  # Note (Andy): this is not going to be scalable. Will need ElasticSearch at some point
+  search_fields = ('notes', '^first_name', '^last_name', 'company')
+
+  def get_queryset(self):
+    try:
+      user = self.request.user
+      creator = user.customer_profile
+      company = creator.company
+      if company and company.allow_contact_sharing:
+        queryset = Contact.objects.filter(creator__company_id=company.id)
+      else:
+        queryset = Contact.objects.filter(creator=creator)
+      
+      investment_size = self.request.query_params.get('investment_size', None)
+      if investment_size is not None:
+        queryset = queryset.filter(
+          minimum_investment_size__lte=investment_size,
+          maximum_investment_size__gte=investment_size
+        )
+
+      target_return = self.request.query_params.get('target_return', None)
+      if target_return is not None:
+        queryset = queryset.filter(
+          minimum_irr_return__lte=target_return,
+          maximum_irr_return__gte=target_return
+        )
+    except:  # TODO (Andy): break down the exceptions and log them
+      queryset = Contact.objects.none()
+
+    return queryset
+
+
 class FollowUpViewSet(viewsets.ModelViewSet):
   queryset = FollowUp.objects.all()
   serializer_class = FollowUpSerializer
